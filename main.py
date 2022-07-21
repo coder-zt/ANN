41b737746f0d64b94692b632afe3b428e0189c2b
coder-zt <691076379@qq.com>
2022-07-21 17:41:57 +0800

    代码分析

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ import numpy as np
 import random
 import mnist_loader
 
+
+# sigmoid函数
 def sigmoid(z):
     return 1.0/(1 + np.exp(-z))
+# sigmoid(x) = 1/[1 + e^(-x)] = e^x/(1 + e^x)
+# [e^x（1 + e^x）- e^2x]/(1 + e^2x + 2e^x) = e^x/(1 +e^x)^2 = e^x/(1 + e^x) * 1/(1 + e^x) 
+# = e^x/(1 + e^x) * [1 - e^x/(1 + e^x)] = sigmoid(x) * (1 - sigmoid(x))
+# 
 
 
+
+#sigmoid函数的导函数
 def sigmoid_prime(z):
     return sigmoid(z) * (1 - sigmoid(z))
 
@@ -59,15 +67,17 @@ class NetWork(object):
     def update_mini_batch(self, mini_batche, eta):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
-        # print(mini_batche)
         for x,y in mini_batche:
+            # 反向传播获取参数变化的delta
             delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            # 统计该数据块的delta
             nabla_b = [nb + dnb for nb,dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [nw + dnw for nw,dnw in zip(nabla_w, delta_nabla_w)]
-            a,b = zip(*mini_batche)
-            size = len(a)
-            self.weights = [w - (eta/size) * nw for w,nw in zip(self.weights, nabla_w)]
-            self.biases = [b - (eta/size) * nb for b,nb in zip(self.biases, nabla_b)]
+        a,b = zip(*mini_batche)
+        size = len(a)
+        # 结合学习率修改网络中的参数
+        self.weights = [w - (eta/size) * nw for w,nw in zip(self.weights, nabla_w)]
+        self.biases = [b - (eta/size) * nb for b,nb in zip(self.biases, nabla_b)]
             
     def backprop(self, x, y):
         # print("x ===> " + str(x))
@@ -78,23 +88,27 @@ class NetWork(object):
         activations = [x]
         zs = []
         # 前向计算
+        # 输入层     隐藏层        偏置   中间结果
+        # (784 * 1) （30*784） (30 * 1)  （30 * 1)
+        # 隐藏层     输出层        偏置   最终结果
+        # (30 * 1) （10 * 30） (10 * 1)  （10 * 1） 
         for b,w in zip(self.biases, self.weights):
             z = np.dot(w, activation) + b
             zs.append(z)
             activation = sigmoid(z)
             activations.append(activation)
-        
-        # print("z ===> " + str(activations[-1]))
-        # activations = np.array(activations)
+        # x -> y
+        #  sigmoid(wx + b) = z
+        #  diff = 1/2 * (z - y)^2 = 1/2 * (sigmoid(wx + b) - y)^2
+        #  delta = (sigmoid(wx + b) -y) * sigmoid_prime(wx + b) = db
+        # dw = (sigmoid(wx + b) -y) * sigmoid_prime(wx + b) * x
+        # ??: self.cost_derivative(activations[-1], y) 为什么求导会加入这部分 ==> (sigmoid(wx + b) -y) 使用的损失函数是：1/2 * (z - y)^2
         # 后向计算 
         delta = self.cost_derivative(activations[-1], y)*sigmoid_prime(zs[-1])
-        # print("delta ===> "  + str(delta))
-        nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].T)
         for l in range(2, self.num_layers):
             z = zs[-l]
             sp = sigmoid_prime(z)
-            # delta  = np.dot(delta, self.weights[-l + 1]) * sp
             delta = np.dot(self.weights[-l+1].T, delta) * sp
             nabla_b[-l] = delta
             # print("activations[-l-1] ===> " + str(activations[-l-1]))
@@ -121,10 +135,10 @@ class NetWork(object):
             # 打乱训练数据
             zipTrainingData = shuffleZipData(zipTrainingData)
             # 取部分数据
-            # mini_batches = [zipTrainingData[k:k+mini_batch_size] for k in range(0, n, mini_batch_size)]
             mini_batches = []
             count = 0
             mini_batche = []
+            # 将训练的数据集切分为多个总数为mini_batch_size的小数据集，然后依次加入计算并更新网络中的参数
             for zipData in zipTrainingData:
                 # print("zipdata foreach")
                 count += 1
@@ -139,8 +153,7 @@ class NetWork(object):
                     mini_batches.append(zip(a,b))
                     mini_batche.clear()
                     count = 0
-            
-            print("本次训练" + str(len(mini_batches)))
+            # 依次加入计算并更新网络中的参数
             for mini_batche in mini_batches:
                 self.update_mini_batch(mini_batche,eta)
             if test_data:
@@ -163,21 +176,18 @@ def printNumPic(numArray):
     
 
 # test()
-# training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-# trainingData = DatePackage(training_data)
-# testData = DatePackage(test_data) #72104
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+trainingData = DatePackage(training_data)
+testData = DatePackage(test_data) #72104
 # # training_X =  [[[1],[2]],[[2],[3]],[[4],[5]],[[1],[2]],[[2],[3]],[[4],[5]],[[1],[2]],[[2],[3]],[[4],[5]],[[1],[2]],[[2],[3]],[[4],[5]],[[1],[2]],[[2],[3]],[[4],[5]]]
 # # training_Y =  []
 # # for x in training_X:
 # #     training_Y.append([[x[0][0] / x[1][0]]])
 n = NetWork([784,30,10])
-for i in n.weights:
-    print(len(i))
-    for j in i:
-        print(len(j))
-
+# weights: {(784 * 30), (30 * 10)}
+print(n.biases)
 # print(n.biases)
-# n.SGD(trainingData, 40,10,2.0, testData)
+# n.SGD(trainingData, 40,10,3.0, testData)
 # for i in range(0,10):
 #     printNumPic(testData.input[i * i * i])
 #     res = n.feedForward(testData.input[i * i * i])
